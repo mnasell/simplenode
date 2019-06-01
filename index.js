970f4acd9b97245e4e45d5015382c66d0cc09060
Markus Nasellu <markus@mnasell.de>
2019-06-01 09:20:07 +0200

    initial commitÃ

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+var stdio = require('stdio'); 
+var ops = stdio.getopt( 
+          { 
+            'port': {key: 'p', args: 1, mandatory: true, description: 'Port # of the service'}           
+           // 'check': {key: 'c', args: 2, description: 'What this option means'}
+           //, 'map': {key: 'm', description: 'Another description'}
+           //, 'kaka': {args: 1, mandatory: true}
+           //, 'ooo': {key: 'o'} 
+});
+
 const express = require('express');
 const app = express();
-app.listen(3000, () => console.log('listening on mnasell.de:3000')); 
+app.listen(ops.port, () => console.log('listening on mnasell.de:' + ops.port)); 
 app.use(express.static('public'));
-
 
